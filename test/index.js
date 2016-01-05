011d0af90e35da66f3389f5af64b160395ed3180
Matheus Brasil <matheus.brasil10@gmail.com>
2016-01-05 02:37:54 -0200

    Add initial structure

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import sensualizator from '../lib';
+
+describe('sensualizator', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
